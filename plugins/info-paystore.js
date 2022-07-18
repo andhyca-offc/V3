afc90fd2c32d657eced948e60e0f25c8584aaadb
andhyca-offc <108988560+andhyca-offc@users.noreply.github.com>
2022-07-18 11:37:22 +0700

    Update info-paystore.js

--- plugins/info-paystore.js
+++ plugins/info-paystore.js
@@ -2,100 +2,78 @@ let handler = async (m, { conn }) => {
 	//-----PRICE
 //sewa
 let sh = '5'
-let sn = '10'
-let ss = '25'
-let sp = '40'
-let sv = '120'
+let sn = '15'
+let ss = '23'
+let sp = '35'
+let sv = '50'
 //premium
 let ph = '5'
-let pn = '15'
-let pp = '45'
-let pv = '65'
-let ppm = '70'
+let pn = '10'
+let pp = '30'
+let pv = '40'
+let ppm = '55'
 let info = `
-*${htki} sᴇᴡᴀ ${htka}*
-┏⫹⫺ *ʜᴇᴍᴀᴛ* 
-┗$ ${sh}k/grup (1 minggu)
+╭━━━━「 *SEWA* 」
+┊⫹⫺ *Hemat:* _${sh}k/grup (1 minggu)_
+┊⫹⫺ *Normal:* _${sn}k/grup (1 bulan)_
+┊⫹⫺ *Standar:* _${ss}k/grup (5 bulan)_
+┊⫹⫺ *Pro:* _${sp}k/grup (12 bulan)_                                                      
+┊⫹⫺ *Vip:* = _${sv}k/grup (Permanent & 1 bulan *Premium*)_
+╰═┅═━––––––๑
+╭━━━━「 *PREMIUM* 」
+┊⫹⫺ *Hemat:* _${ph}k (1 minggu)_
+┊⫹⫺ *Normal:* _${pn}k (1 bulan)_
+┊⫹⫺ *Pro:* _${pp}k (4 bulan)_
+┊⫹⫺ *Vip:* _${pv}k (8 bulan)_                                               
+┊⫹⫺ *Permanent:* = _${ppm}k (Unlimited)_
+╰═┅═━––––––๑
+*⫹⫺ PAYMENT:*
+• *Pulsa:* [${ppulsa}]
+• *Dana:* [${pdana}]
+• *GoPay:* [${pgopay}]
 
-┏⫹⫺ *ɴᴏʀᴍᴀʟ* 
-┗$ ${sn}k/grup (1 bulan)
-
-┏⫹⫺ *sᴛᴀɴᴅᴀʀ* 
-┗$ ${ss}k/grup (2 bulan)
-
-┏⫹⫺ *ᴘʀᴏ* 
-┗$ ${sp}k/grup (3 bulan)
-
-┏⫹⫺ *ᴘᴇʀᴍᴀɴᴇɴ* 
-┗$ ${sv}k/grup (Permanen)
-
-──···────────────────···──
-
-*${htki} ᴘʀᴇᴍɪᴜᴍ ${htka}*
-┏⫹⫺ *ʜᴇᴍᴀᴛ* 
-┗$ ${ph}k (1 minggu)
-
-┏⫹⫺ *ɴᴏʀᴍᴀʟ* 
-┗$ ${pn}k (1 bulan)
-
-┏⫹⫺ *ɢᴏᴏᴅ* 
-┗$ ${pp}k (3 bulan)
-
-┏⫹⫺ *ᴘʀᴏ* 
-┗$ ${pv}k (5 bulan)                                            
-
-*📦 Kalau mau dapat discon cek website TokoBot*
-Link: https://toko.ly/TokoBot
-
-–––––– *ᴋᴇʙɪᴊᴀᴋᴀɴ* ––––––
+–––––– *🐾 Kebijakan* ––––––
 🗣️: Kak, Kok harganya mahal banget?
-💬: Mau tawar menawar? boleh, silahkan chat owner aja.. max tawar 5k
-
+💬: Ada harga ada kualitas kak, mau tawar menawar? boleh, silahkan chat owner aja (maximal 4K)
 🗣️: Scam ga nih kak?
-💬: Enggalah, Owner 100% Tepati janji #STAYHALAL
+💬: Enggalah
 
-▌│█║▌║▌║║▌║▌║█│▌
-`
+║▌│█║▌│ █║▌│█│║▌║`
 const sections = [
    {
-	title: `✃ sᴇᴡᴀ`,
+	title: `${htjava} SEWA ✦-------`,
 	rows: [
-	    {title: "𝗛𝗘𝗠𝗔𝗧", rowId: '.pay', description: '𝗣𝗿𝗶𝗰𝗲: ' + sh + 'k (1 minggu)' },
-	    {title: "𝗡𝗢𝗥𝗠𝗔𝗟", rowId: '.pay', description: '𝗣𝗿𝗶𝗰𝗲: ' + sn + 'k (1 bulan)' },
-	{title: "𝗦𝗧𝗔𝗡𝗗𝗔𝗥", rowId: '.pay', description: '𝗣𝗿𝗶𝗰𝗲: ' + ss + 'k (2 bulan)' },
-	{title: "𝗣𝗥𝗢", rowId: '.pay', description: '𝗣𝗿𝗶𝗰𝗲: ' + sp + 'k (3 bulan)' },
-	{title: "🔖 𝗩𝗜𝗣", rowId: '.pay', description: '𝗣𝗿𝗶𝗰𝗲: ' + sv + 'k (Permanen)' },
+	    {title: "🔖 𝗛𝗘𝗠𝗔𝗧", rowId: '.order *Paket:* HEMAT • Sewa', description: '𝗣𝗿𝗶𝗰𝗲: ' + sh + 'k (1 minggu)' },
+	    {title: "🔖 𝗡𝗢𝗥𝗠𝗔𝗟", rowId: '.order *Paket:* NORMAL • Sewa', description: '𝗣𝗿𝗶𝗰𝗲: ' + sn + 'k (1 bulan)' },
+	{title: "🔖 𝗦𝗧𝗔𝗡𝗗𝗔𝗥", rowId: '.order *Paket:* STANDAR • Sewa', description: '𝗣𝗿𝗶𝗰𝗲: ' + ss + 'k (5 bulan)' },
+	{title: "🔖 𝗣𝗥𝗢", rowId: '.order *Paket:* PRO • Sewa', description: '𝗣𝗿𝗶𝗰𝗲: ' + sp + 'k (12 bulan)' },
+	{title: "🔖 𝗩𝗜𝗣", rowId: '.order *Paket:* VIP • Sewa', description: '𝗣𝗿𝗶𝗰𝗲: ' + sv + 'k (Permanent)' },
 	]
     }, {
-    title: `✃ ᴘʀᴇᴍɪᴜᴍ`,
+    title: `${htjava} PREMIUM ✦-------`,
 	rows: [
-	    {title: "𝗛𝗘𝗠𝗔𝗧", rowId: '.pay', description: '𝗣𝗿𝗶𝗰𝗲: ' + ph + 'k (1 minggu)' },
-	    {title: "𝗡𝗢𝗥𝗠𝗔𝗟", rowId: '.pay', description: '𝗣𝗿𝗶𝗰𝗲: ' + pn + 'k (1 bulan)' },
-	{title: "𝗚𝗢𝗢𝗗", rowId: '.pay', description: '𝗣𝗿𝗶𝗰𝗲: ' + pp + 'k (3 bulan)' },
-	{title: "𝗣𝗥𝗢", rowId: '.pay', description: '𝗣𝗿𝗶𝗰𝗲: ' + pv + 'k (5 bulan)' },
+	    {title: "🌟 𝗛𝗘𝗠𝗔𝗧", rowId: '.order *Paket:* HEMAT • Premium', description: '𝗣𝗿𝗶𝗰𝗲: ' + ph + 'k (1 minggu)' },
+	    {title: "🌟 𝗡𝗢𝗥𝗠𝗔𝗟", rowId: '.order *Paket:* NORMAL • Premium', description: '𝗣𝗿𝗶𝗰𝗲: ' + pn + 'k (1 bulan)' },
+	{title: "🌟 𝗣𝗥𝗢", rowId: '.order *Paket:* PRO • Premium', description: '𝗣𝗿𝗶𝗰𝗲: ' + pp + 'k (4 bulan)' },
+	{title: "🌟 𝗩𝗜𝗣", rowId: '.order *Paket:* VIP • Premium', description: '𝗣𝗿𝗶𝗰𝗲: ' + pv + 'k (8 bulan)' },
+	{title: "🌟 𝗣𝗘𝗥𝗠𝗔𝗡𝗘𝗡𝗧", rowId: '.order *Paket:* PERMANENT • Premium', description: '𝗣𝗿𝗶𝗰𝗲: ' + ppm + 'k (UNLIMITED)' },
 	]
-    }, {
-    title: `✃ INFO`,
-	rows: [
-	    {title: "📊 Discount", rowId: '.diskon', description: 'Discount Pembayaran' },
-	    ]
-        },
+    },
 ]
 
 const listMessage = {
-  text: ' ',
-  footer: info,
-  title: null,
-  buttonText: "ʙ ᴜ ʏ",
+  text: info,
+  footer: botdate,
+  title: wm,
+  buttonText: "Click Here!",
   sections
 }
-await conn.sendMessage(m.chat, listMessage, { quoted: m})
+await conn.sendMessage(m.chat, listMessage)
 //conn.sendHydrated(m.chat, info, wm, null, sgc, "🌎 Group Official", null,null, [['Owner','.owner']], m)
 }
 
 handler.help = ['sewa', 'premium']
 handler.tags = ['main']
 handler.command = /^(sewa(bot)?|premium)$/i
-handler.private = true
 
 export default handler
